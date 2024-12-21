fa5a02432ff4e63d1107dc2d7c8c31ff05dcc170
Ziad Ali <ziadfox88@gmail.com>
2024-12-21 12:47:26 +0200

    module 1 done

--- blog/views.py
+++ blog/views.py
@@ -8,6 +8,10 @@ def post_list(request):
     posts = Post.objects.all()
     return render(request, 'post_list.html', {'posts': posts})
 
-def post_detail(request, id):
-    post = get_object_or_404(Post, id=id , status=Post.Status.PUBLISHED) 
+def post_detail(request,year, month, day, post):
+    post = get_object_or_404(Post, status=Post.Status.PUBLISHED,
+                            publish__year=year,
+                            publish__month=month,
+                            publish__day=day,
+                            slug=post) 
     return render(request, 'post_detail.html', {'post': post})
