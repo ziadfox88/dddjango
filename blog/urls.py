32dac311f688705ece558b26bcb0f51ec03425c0
Ziad Ali <ziadfox88@gmail.com>
2024-12-23 20:03:28 +0200

    add class views

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path,include
+from django.urls import path
 from . import views
 
 
 
 urlpatterns = [
-    path('', views.post_list , name='post_list'),
+    #path('', views.post_list , name='post_list'),
+    path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail , name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,21 +2,29 @@ from django.shortcuts import render ,get_object_or_404
 from django.http import HttpResponse,Http404
 from .models import Post
 from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
+from django.views.generic import ListView
 
 
 # Create your views here.
-def post_list(request):
-    post_list = Post.objects.all()
-    paginator = Paginator(post_list, 2)
-    page_number = request.GET.get('page',1)
+#def post_list(request):
+    #post_list = Post.objects.all()
+    #paginator = Paginator(post_list, 2)
+    #page_number = request.GET.get('page',1)
     
-    try:
-        posts = paginator.page(page_number)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages) 
-    except PageNotAnInteger:
-        posts = paginator.page(1)
-    return render(request, 'post_list.html', {'posts': posts})
+    #try:
+        #posts = paginator.page(page_number)
+    #except EmptyPage:
+        #posts = paginator.page(paginator.num_pages) 
+    #except PageNotAnInteger:
+        #posts = paginator.page(1)
+    #return render(request, 'post_list.html', {'posts': posts})
+    
+    
+class PostListView(ListView):
+    model = Post
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'post_list.html'
 
 
 def post_detail(request,year, month, day, post):
